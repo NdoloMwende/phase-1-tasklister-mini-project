f2eedb0245b23f3c1b9f3d99ad238a258bb0e35c
Ndolo Mercy <mercymwendend444@gmail.com>
2025-07-12 13:24:41 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // console.log("Ready");
+  const form =document.querySelector("form");
+  const input=document.querySelector("#new-task-description");
+  // console.log(form);
+  // console.log(input);
+
+  form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  buildList(document.querySelector("#new-task-description").value);
+
+  form.reset();
 });
+ 
+});
+function buildList(task){
+  // console.log(task);
+  let li=document.createElement("li");
+  li.textContent=task;
+
+  let btn=document.createElement("button");  
+  btn.textContent= " Delete"
+  btn.addEventListener('click',deleteTask)
+
+  li.appendChild(btn);
+
+  document.querySelector("#tasks").appendChild(li)
+  // console.log(p);
+}
+function deleteTask(e){
+  e.target.parentNode.remove()
+  // console.log(e.target.parentNode);
+};
+function selectpriority(task){
+  let dropdown=document.createElement("section");
+}
